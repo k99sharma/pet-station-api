ef27fd8884e3c6a3af71359bfe712b1ecfe6e05e
ghost_32 <kalash.strt@gmail.com>
2022-07-18 01:58:33 +0530

    feat($linter): add linters and prettier
    Added eslint and prettier configuration file
     - eslint is configured for linting
     - prettier is configured for formatting project

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
 // importing modules
-const cluster = require('cluster');
-const os = require('os');
-const runServer = require('./app');
+const cluster = require('cluster')
+const os = require('os')
+const runServer = require('./app')
 
 // condition to check if current process is master.
-if(cluster.isMaster){
-    
+if (cluster.isMaster) {
     // condition to check if current environment is production or not.
-    if(process.env.NODE_ENV === 'production'){
-        
+    if (process.env.NODE_ENV === 'production') {
         // get total CPU cores count.
-        const cpuCount = os.cpus().length;
+        const cpuCount = os.cpus().length
 
         // spawn a worker for every available core.
-        for(let cpu=0; cpu < cpuCount; cpu++){
-            cluster.fork();
+        for (let cpu = 0; cpu < cpuCount; cpu++) {
+            cluster.fork()
         }
-    }else{
-        cluster.fork();
+    } else {
+        cluster.fork()
     }
-}else{
-
+} else {
     // run server.
-    runServer();
+    runServer()
 }
 
 // create new worker if any one is dead.
-cluster.on('exit', function(worker){
-    console.warn(`Worker ${worker.id} died.`);
-    console.log('Starting new worker!');
-    
+cluster.on('exit', function (worker) {
+    console.warn(`Worker ${worker.id} died.`)
+    console.log('Starting new worker!')
+
     // starting new worker.
-    cluster.fork();
+    cluster.fork()
 })
