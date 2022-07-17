4a4a6b17fbf3c577df5f5f6eee9a74bb80785e9e
ghost_32 <kalash.strt@gmail.com>
2022-07-18 00:47:22 +0530

    add($app): app.js file to run server
    Added app.js to implement
     - routes
     - setting up middlewares
     - routes
     - headers
     - start server function

---
+++ app.js
@@ -0,0 +1,84 @@
+// import modules
+const express = require('express');
+const compression = require('compression');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+// app
+const app = express();
+
+// configuring environment variables
+require('dotenv').config();
+
+// setting up middleware 
+app.use(compression());     // compression middleware
+app.use(morgan('dev'));     // morgan middleware
+app.use(helmet());          // helmet middleware
+
+// cors
+app.use(cors(               
+    { exposedHeaders : "x-auth-token" }
+));
+
+// urlencoded parsing
+app.use(express.urlencoded(
+    {
+        limit : '50mb',
+        extended : true,
+        parameterLimit : 1000000
+    }
+));
+
+// json parsing
+app.use(express.json(
+    {
+        limit: '50mb',
+        extended: true,
+        parameterLimit: 1000000
+    }
+));
+
+// setting up production console errors
+if(NODE_ENV === 'production')
+    console.log = console.warn = console.error = () => {};
+
+// setting up routes
+
+// error handlers
+
+// allowing headers
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE'
+    );
+
+    next();
+});
+
+/** 
+ * function to start the server.
+ * 
+ * @param { void }
+ * @return { void } 
+ */
+
+function startServer(){
+    try{
+        app.listen(PORT || 3000);
+        console.info(`${ NODE_ENV } server is up and running on PORT: ${ PORT }.`);
+    }
+    catch(err){
+        console.info('Error in running server.');
+    }
+}
+
+// exporting functions
+module.exports = startServer();
