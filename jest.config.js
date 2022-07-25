b88b9123ee6e022d51c5fa417f92b9dccfaa95b6
ghost_32 <kalash.strt@gmail.com>
2022-07-25 22:31:39 +0530

    add($jest): add jest library and configurations
    - jest library is added
    - jest config file is setup to look for tests in `tests` folder
    - eslint config file is configured for jest code

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+// jest configuration files.
+const config = {
+    verbose: true,
+    testMatch: ['**/tests/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)']
+}
+
+module.exports = config;
---
+++ tests/modelHelper.js
@@ -0,0 +1,14 @@
+// importing functions
+// const modelHelper = require('../server/utilities/modelHelper');
+
+
+/**
+ * tests for uniqueIdGenerator function.
+ * 
+ * function returns an unique id based on first name and last name.
+ * it returns the id in string format.
+ */
+
+test('sum test', ()=>{
+    expect(2+2).toBe(4);
+});
