8390787eb96e7644d879837a0cf245b0dc5d5b15
ghost_32 <kalash.strt@gmail.com>
2022-08-03 02:42:47 +0530

    update(): auth middleware is updated to check token in redis

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -10,26 +10,34 @@ const { sendError } = require('../utils/errorHelper');
 // importing status codes
 const { NOT_AUTHORIZED } = require('../utils/statusCodes');
 
+// redis client
+const { getClient } = require('../configs/redisConnection');
+const client = getClient();
+
 // authentication for all users
 const allAuth = (req, res, next) => {
     // getting token
     const token = req.header("x-auth-token");
 
     // if no token found send error
-    if(!token)
+    if (!token)
         return sendError(
-            res, 
-            "Access Denied. No token provided",
+            res,
+            "Access Denied. No token provided.",
             NOT_AUTHORIZED
         );
+
+    // check if token is in cache or not
+    if (client.get(token) !== 'true')
+        return sendError(res, 'Access Denied. Invalid token.', NOT_AUTHORIZED);
 
     // decoding payload
     let decodedPayload;
 
-    try{
+    try {
         decodedPayload = jwt.verify(token, JWT_PRIVATE_KEY)
     }
-    catch(err){
+    catch (err) {
         return sendError(res, err, NOT_AUTHORIZED)
     }
 
@@ -43,25 +51,29 @@ const adminAuth = (req, res, next) => {
     const token = req.header("x-auth-token");
 
     // if no token found send error
-    if(!token)
+    if (!token)
         return sendError(
             res,
             "Access Denied. No token provided",
             NOT_AUTHORIZED
         );
 
+    // check if token is in cache or not
+    if (client.get(token) !== 'true')
+        return sendError(res, 'Access Denied. Invalid token.', NOT_AUTHORIZED);
+
     // decoding payload
     let decodedPayload;
-    
-    try{
-        decodedPayload= jwt.verify(token, JWT_PRIVATE_KEY);
+
+    try {
+        decodedPayload = jwt.verify(token, JWT_PRIVATE_KEY);
     }
-    catch(err){
+    catch (err) {
         return sendError(res, err, NOT_AUTHORIZED);
     }
 
     // check for admin
-    if(decodedPayload.role === 'admin'){
+    if (decodedPayload.role === 'admin') {
         req.user = decodedPayload;
         return next();
     }
