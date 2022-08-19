fd26869ac75b0a07aa646040f666fcc9941a4c1b
ghost_32 <kalash.strt@gmail.com>
2022-08-20 00:41:25 +0530

    fix($service): email service is changed
    Service routes are removed
     - email service route is removed
     - email service is changed to a function to send emails based on cases
     - email service is added in login route

--- server/services/email/service.js
+++ server/services/email/service.js
@@ -12,6 +12,7 @@ const {
  * Email can be modified based on provided options.
  * 
  * @param { object } options 
+ * @return { object } { error, msg }
  */
 async function emailMain(options) {
     // create reusable transporter object using the default SMTP transport
@@ -34,11 +35,71 @@ async function emailMain(options) {
         text: options.text, // plain text body
     });
 
-    console.log(info);
+    // TODO: wtf to do with rejected requests -> info.rejected
+
+    // if email cannot be sent
+    if(info.rejected.length != 0)
+        return {
+            error: true,
+            msg: 'Request not complete.'
+        }
+
+    return {
+        error: false,
+        msg: 'Request complete.'
+    }
 }
 
+/**
+ * Function to send case specific emails.
+ * Different Templates are used based on different cases.
+ * 
+ * case 1: login
+ * case 2: logout
+ * case 3: email Verification v1.2
+ * case 4: password reset v1.2
+ * case 5: signup
+ * 
+ * @params { String, Object } case number, userData
+ * @returns { Object } result
+ */
+async function emailService(caseId, userData){
+    const options = {};
+
+    // TODO: need to complete all the cases
+    if(caseId === 'login'){
+        options.mailTo = userData.email;
+        options.subject = 'User logged in.',
+        options.text = 'Pet station log in.'
+    }
+    else if(caseId === 'logout'){
+        options.mailTo = userData.email;
+        options.subject = 'User logout.';
+        options.text = 'Pet Station logout.';
+    }
+    else if(caseId === 'signup'){
+        options.mailTo = userData.email;
+        options.subject = 'Pet Station signup';
+        options.text = 'Pet Station signup';
+    }
+
+    // send email
+    const res = await emailMain(options);
+
+    if(res.error)
+        return {
+            error: true,
+            msg: 'Email is not sent.'
+        }
+
+    return {
+        error: false,
+        msg: 'Email is sent.'
+    }
+}
 
 // exporting function
 module.exports = {
-    emailMain
+    emailMain,
+    emailService
 }
