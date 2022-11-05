4e003446b7cfd6d6c38faaea8bfa0694b8f132e4
Kalash Sharma <kalash.strt@gmail.com>
2022-11-06 00:09:26 +0530

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -59,7 +59,7 @@ const userLogin = async (req, res) => {
     const { email, password } = req.body
 
     // check if email is valid
-    const user = await User.findOne({ email: email }).lean();
+    const user = await User.findOne({ email: email })
     if (!user) return sendError(res, 'Invalid email or password', FORBIDDEN)
 
     // check if password is valid
@@ -89,7 +89,7 @@ const extendToken = async (req, res) => {
     const userId = req.user.userId
 
     // get user
-    const user = await User.findOne({ userId: userId }).lean();
+    const user = await User.findOne({ userId: userId })
 
     // generate new token
     const generatedToken = await user.generateAuthToken()
