15a8ced8a6514ecd611f11f3c25252691d0bc200
ghost32 <kalash.strt@gmail.com>
2022-11-25 22:14:21 +0530

    feat($error): add error handlers
     - error handlers are added
     - response format is created

---
+++ server/utilities/statusCodes.js
@@ -0,0 +1,14 @@
+// status codes
+const statusCodes = {
+    OK: 200,
+    UPDATED: 204,
+    BAD_REQUEST: 400,
+    NOT_AUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    NOT_ACCEPTABLE: 406,
+    CONFLICT: 409,
+    SERVER_ERROR: 500
+}
+
+export default statusCodes;
