79784d6bd905d38869b0961419901a0f4b89dced
Kalash Sharma <kalash.strt@gmail.com>
2022-12-26 19:34:33 +0530

    Update socketServer.js

--- server/websocket/socketServer.js
+++ server/websocket/socketServer.js
@@ -11,10 +11,9 @@ import socketEvent from './event.js';
 export default function socketServer(server) {
     const io = new Server(server, {
         cors: {
-            origin: "https://localhost:3000",
-            methods: ["GET", "POST"],
-            credentials: true
-         }
+            origin: "http://localhost:3000",
+            methods: ["GET", "POST"]
+        }
     });  // socket server
 
     // middleware
